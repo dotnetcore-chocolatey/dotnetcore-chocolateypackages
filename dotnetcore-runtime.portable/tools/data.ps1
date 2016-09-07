2eba68cb0f39b72894638ffb932e5023fc07cae3
Jakub Bereżański <kuba@berezanscy.pl>
2016-09-08 00:38:25 +0200

    update dotnetcore-runtime.portable to 1.0.1
    The systemwide installer for 1.0.1 has not been released yet.

--- dotnetcore-runtime.portable/tools/data.ps1
+++ dotnetcore-runtime.portable/tools/data.ps1
@@ -1,10 +1,10 @@
 ﻿@{
     PackageName = 'dotnetcore-runtime.portable'
-    Version = '1.0.0'
-    Url = 'https://download.microsoft.com/download/8/4/E/84EA9F4F-0E3A-4B41-A18A-36D51B06CBED/dotnet-win-x86.1.0.0.zip'
-    Checksum = '4F5A4CEF9BCEB2D66613F3C4163B69C0111ED163'
+    Version = '1.0.1'
+    Url = 'https://download.microsoft.com/download/B/0/0/B00543E8-54D9-4D4A-826B-84348956AA75/dotnet-win-x86.1.0.1.zip'
+    Checksum = '8AB9AF0AE0D8BF9A3B74E72D64CF779D816DFAF0'
     ChecksumType = 'sha1'
-    Url64 = 'https://download.microsoft.com/download/8/4/E/84EA9F4F-0E3A-4B41-A18A-36D51B06CBED/dotnet-win-x64.1.0.0.zip'
-    Checksum64 = 'D96C58EED3C6E40E066E38C5ABF6DEC71F85CCDF'
+    Url64 = 'https://download.microsoft.com/download/B/0/0/B00543E8-54D9-4D4A-826B-84348956AA75/dotnet-win-x64.1.0.1.zip'
+    Checksum64 = '9C65CFD6D95FAB986C3441C41D37F388754B8BC1'
     ChecksumType64 = 'sha1'
 }
