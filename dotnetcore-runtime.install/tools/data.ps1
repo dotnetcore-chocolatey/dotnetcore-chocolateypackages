6e6b45ce6e34b164f8092845683ceb27ca5357d7
Jakub Bereżański <kuba@berezanscy.pl>
2016-09-15 22:20:29 +0200

    update to .NET Core 1.0.1 binary release for Runtime and Server Hosting

--- dotnetcore-runtime.install/tools/data.ps1
+++ dotnetcore-runtime.install/tools/data.ps1
@@ -1,13 +1,13 @@
 ﻿@{
     PackageName = 'dotnetcore-runtime.install'
-    Version = '1.0.0'
-    Url = 'https://download.microsoft.com/download/A/3/8/A38489F3-9777-41DD-83F8-2CBDFAB2520C/packages/DotNetCore.1.0.0-Runtime-x86.exe'
-    Checksum = '88581A972CBBA8CD0954987FDDCC274212EB315D'
-    ChecksumType = 'sha1'
-    Url64 = 'https://download.microsoft.com/download/A/3/8/A38489F3-9777-41DD-83F8-2CBDFAB2520C/packages/DotNetCore.1.0.0-Runtime-x64.exe'
-    Checksum64 = '0953E863F28809901663BD86B0150EAE977A31F4'
-    ChecksumType64 = 'sha1'
-    ApplicationName = 'Microsoft .NET Core 1.0.0 - Runtime (*)'
-    UninstallerName = 'dotnet-win-*.1.0.0.exe'
+    Version = '1.0.1'
+    Url = 'https://download.microsoft.com/download/E/D/7/ED7A0105-03D6-4527-AEA4-8308C3B4D337/DotNetCore.1.0.1-Runtime-x86.exe'
+    Checksum = 'C60D263983CB38F42D7E040F0D5DE6B1CC1B6D9D76D6E39409CBFD745ADBD156'
+    ChecksumType = 'sha256'
+    Url64 = 'https://download.microsoft.com/download/E/D/7/ED7A0105-03D6-4527-AEA4-8308C3B4D337/DotNetCore.1.0.1-Runtime-x64.exe'
+    Checksum64 = 'F77BE2ABBCB35B6EB8CEB765760A59F49CF8F3A19DB85C5908151AD5E8FF2714'
+    ChecksumType64 = 'sha256'
+    ApplicationName = 'Microsoft .NET Core 1.0.1 - Runtime (*)'
+    UninstallerName = 'dotnet-win-*.1.0.1.exe'
     AdditionalInstallerArguments = $null
 }
