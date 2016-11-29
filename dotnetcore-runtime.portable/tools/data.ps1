3236d75744da3f31232596dd38ae67aac908dfbd
Jakub Bereżański <kuba@berezanscy.pl>
2016-11-29 23:05:11 +0100

    update dotnetcore-runtime.portable to 1.1.0 and SHA256

--- dotnetcore-runtime.portable/tools/data.ps1
+++ dotnetcore-runtime.portable/tools/data.ps1
@@ -1,10 +1,10 @@
 ﻿@{
     PackageName = 'dotnetcore-runtime.portable'
-    Version = '1.0.1'
-    Url = 'https://download.microsoft.com/download/B/0/0/B00543E8-54D9-4D4A-826B-84348956AA75/dotnet-win-x86.1.0.1.zip'
-    Checksum = '8AB9AF0AE0D8BF9A3B74E72D64CF779D816DFAF0'
-    ChecksumType = 'sha1'
-    Url64 = 'https://download.microsoft.com/download/B/0/0/B00543E8-54D9-4D4A-826B-84348956AA75/dotnet-win-x64.1.0.1.zip'
-    Checksum64 = '9C65CFD6D95FAB986C3441C41D37F388754B8BC1'
-    ChecksumType64 = 'sha1'
+    Version = '1.1.0'
+    Url = 'http://download.microsoft.com/download/A/F/6/AF610E6A-1D2D-47D8-80B8-F178951A0C72/Binaries/dotnet-win-x86.1.1.0.zip'
+    Checksum = '6E08A9753F96ED2087D189DD1ADEA7611E90BD88614157C1FE51CF397A967ED9'
+    ChecksumType = 'sha256'
+    Url64 = 'http://download.microsoft.com/download/A/F/6/AF610E6A-1D2D-47D8-80B8-F178951A0C72/Binaries/dotnet-win-x64.1.1.0.zip'
+    Checksum64 = 'CC66A190EDC10866494DF9E72DBAF4B872C83045E26431161131459AB413A08C'
+    ChecksumType64 = 'sha256'
 }
