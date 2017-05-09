90f616816427c1ef6a24561231720c871cfef730
Jakub Bereżański <kuba@berezanscy.pl>
2017-05-09 21:33:39 +0200

    update runtime and windowshosting to 1.1.2

--- dotnetcore-runtime.install/tools/data.ps1
+++ dotnetcore-runtime.install/tools/data.ps1
@@ -1,13 +1,13 @@
 @{
     PackageName = 'dotnetcore-runtime.install'
-    Version = '1.1.1'
-    Url = 'https://download.microsoft.com/download/9/5/1/95198156-644E-4CCE-8DA1-C41F7658510C/dotnet-win-x86.1.1.1.exe'
-    Checksum = '398F164ABB058FC95A06592B1F171287A7E03CACCAAD6220C010E6FA57CFC385'
+    Version = '1.1.2'
+    Url = 'https://download.microsoft.com/download/D/0/2/D028801E-0802-43C8-9F9F-C7DB0A39B344/dotnet-win-x86.1.1.2.exe'
+    Checksum = 'c9bcf2e355e17919b32ee427ce1e5c2f2a9d0cef6729aeb440e20a8b07d4754f'
     ChecksumType = 'sha256'
-    Url64 = 'https://download.microsoft.com/download/9/5/1/95198156-644E-4CCE-8DA1-C41F7658510C/dotnet-win-x64.1.1.1.exe'
-    Checksum64 = 'C0B2344526033907B6F2F0BD3FB0F776C9FC1FD20114075BAF987012E2390E36'
+    Url64 = 'https://download.microsoft.com/download/D/0/2/D028801E-0802-43C8-9F9F-C7DB0A39B344/dotnet-win-x64.1.1.2.exe'
+    Checksum64 = '104b18531cee34c2ba3ac19c7f717fe57791b76240d3c2455f26cdbbf5d8dc22'
     ChecksumType64 = 'sha256'
-    ApplicationName = 'Microsoft .NET Core 1.1.1 - Runtime (*)'
-    UninstallerName = 'dotnet-win-*.1.1.1.exe'
+    ApplicationName = 'Microsoft .NET Core 1.1.2 - Runtime (*)'
+    UninstallerName = 'dotnet-win-*.1.1.2.exe'
     AdditionalArgumentsToInstaller = $null
 }
--- dotnetcore-runtime.portable/tools/data.ps1
+++ dotnetcore-runtime.portable/tools/data.ps1
@@ -1,10 +1,10 @@
 ﻿@{
     PackageName = 'dotnetcore-runtime.portable'
-    Version = '1.1.1'
-    Url = 'https://download.microsoft.com/download/9/5/6/9568826C-E3F6-44A7-9F75-DD8E6AB29543/dotnet-win-x86.1.1.1.zip'
-    Checksum = '0C69428E55782603982B173BC66EB24FBF80F9B01FFDF8A00F012BEAAB3C4653'
+    Version = '1.1.2'
+    Url = 'https://download.microsoft.com/download/D/7/A/D7A9E4E9-5D25-4F0C-B071-210CB8267943/dotnet-win-x86.1.1.2.zip'
+    Checksum = '31e49e6ae491795c99221307dc829bbb57e645d568a94820a031688b6d8df4c6'
     ChecksumType = 'sha256'
-    Url64 = 'https://download.microsoft.com/download/9/5/6/9568826C-E3F6-44A7-9F75-DD8E6AB29543/dotnet-win-x64.1.1.1.zip'
-    Checksum64 = 'B0F7FC902308B98FA8E202081884D310A94A93264A1F5BEEB4632ACCCF2C0BB2'
+    Url64 = 'https://download.microsoft.com/download/D/7/A/D7A9E4E9-5D25-4F0C-B071-210CB8267943/dotnet-win-x64.1.1.2.zip'
+    Checksum64 = 'a4ccb23a6cf2ddf9894cb178ce8e69b5788880a1e3d9d659f26fb914a4bc2988'
     ChecksumType64 = 'sha256'
 }
