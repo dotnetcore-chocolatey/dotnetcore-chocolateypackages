24ea933abc5d5881a59bb0f2a986e9a69d18f015
Jakub Bereżański <kuba@berezanscy.pl>
2017-05-11 23:25:54 +0200

    add aspnetcore-runtimepackagestore Preview 1

---
+++ aspnetcore-runtimepackagestore/tools/data.ps1
@@ -0,0 +1,13 @@
+@{
+    PackageName = 'aspnetcore-runtimepackagestore'
+    Version = '2.0.0-Preview1'
+    Url = 'https://download.microsoft.com/download/4/6/6/4668DB58-0643-48EA-A194-CAD059143696/AspNetCore.2.0.0-Preview1.RuntimePackageStore_x86.exe'
+    Checksum = 'BC113F42E11B084EE2E471A49C927FCA9C3D24784FE59F48DFE87B21CFB29B71'
+    ChecksumType = 'sha256'
+    Url64 = 'https://download.microsoft.com/download/4/6/6/4668DB58-0643-48EA-A194-CAD059143696/AspNetCore.2.0.0-Preview1.RuntimePackageStore_x64.exe'
+    Checksum64 = '09B1F149F66AF6B618D39741C2BAD469D91FCC72FF8BC19D72C299DB3C2B6073'
+    ChecksumType64 = 'sha256'
+    ApplicationName = 'Microsoft ASP.NET Core 2.0.0 Preview 1 - Runtime Package Store'
+    UninstallerName = 'AspNetCore.2.0.0-Preview1.RuntimePackageStore_*.exe'
+    AdditionalArgumentsToInstaller = $null
+}
