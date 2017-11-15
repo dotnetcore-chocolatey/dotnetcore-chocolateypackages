aacfc998b70561a40c242251178de518210cd662
Martin Costello <martin@martincostello.com>
2017-11-15 18:26:48 +0000

    Fix x86 download link
    Update the download link for the x86 version of the portable SDK: https://github.com/dotnet/core/commit/fdbf2e210e83848d2d76687827a1ca14cd6b9ceb.

--- dotnetcore-runtime.portable/tools/data.ps1
+++ dotnetcore-runtime.portable/tools/data.ps1
@@ -1,7 +1,7 @@
 ﻿@{
     PackageName = 'dotnetcore-runtime.portable'
     Version = '2.0.3'
-    Url = 'https://download.microsoft.com/download/5/C/1/5C190037-632B-443D-842D-39085F02E1E8/dotnet-sdk-2.0.3-win-x86.zip'
+    Url = 'https://download.microsoft.com/download/D/7/2/D725E47F-A4F1-4285-8935-A91AE2FCC06A/dotnet-sdk-2.0.3-win-x86.zip'
     Checksum = '37deddbfc1a8ea72da4e864343bb4374bd50d744e1621910de30f8d83eaa3860b14f262750810a7ff66f8ab05a587cd71a935a130acf499257d8d4509e0b3cb2'
     ChecksumType = 'sha512'
     Url64 = 'https://download.microsoft.com/download/D/7/2/D725E47F-A4F1-4285-8935-A91AE2FCC06A/dotnet-sdk-2.0.3-win-x64.zip'
