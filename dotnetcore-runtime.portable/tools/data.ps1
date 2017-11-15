b8414d988dc8de42773a1cd0d3c8b03a783ea3af
martin-costello <martin.costello@just-eat.com>
2017-11-15 10:34:39 +0000

    Add x86 portable runtime link and use SHA-512
    Add the download link for the x86 portable SDK.
    Update hashes to use SHA-512.

--- dotnetcore-runtime.portable/tools/data.ps1
+++ dotnetcore-runtime.portable/tools/data.ps1
@@ -1,10 +1,10 @@
 ﻿@{
     PackageName = 'dotnetcore-runtime.portable'
     Version = '2.0.3'
-    Url = '?'
-    Checksum = '?'
-    ChecksumType = 'sha256'
+    Url = 'https://download.microsoft.com/download/5/C/1/5C190037-632B-443D-842D-39085F02E1E8/dotnet-sdk-2.0.3-win-x86.zip'
+    Checksum = '37deddbfc1a8ea72da4e864343bb4374bd50d744e1621910de30f8d83eaa3860b14f262750810a7ff66f8ab05a587cd71a935a130acf499257d8d4509e0b3cb2'
+    ChecksumType = 'sha512'
     Url64 = 'https://download.microsoft.com/download/D/7/2/D725E47F-A4F1-4285-8935-A91AE2FCC06A/dotnet-sdk-2.0.3-win-x64.zip'
-    Checksum64 = '64556C5454BE49388A73525518F372B003113D3A418B92EA41E1659F1734B045'
-    ChecksumType64 = 'sha256'
+    Checksum64 = '8b4bd2174f413f9043374d0cfa7d4061f0aeedc28c16257e66b99f28454bc0a898c6f52d4cf8349413ae222b21e6f594a8f70c1892844a0416e2be2994af13f1'
+    ChecksumType64 = 'sha512'
 }
