ae676feaa084932b40a827129c88f6ad172349f4
martin-costello <martin.costello@just-eat.com>
2017-12-14 12:32:18 +0000

    .NET Core 2.0.4
    Update for .NET Core 2.0.4.

--- dotnetcore-runtime.install/tools/data.ps1
+++ dotnetcore-runtime.install/tools/data.ps1
@@ -1,13 +1,13 @@
 @{
     PackageName = 'dotnetcore-runtime.install'
-    Version = '2.0.3'
-    Url = 'https://download.microsoft.com/download/5/C/1/5C190037-632B-443D-842D-39085F02E1E8/dotnet-runtime-2.0.3-win-x86.exe'
-    Checksum = '490b3adc7922d54ddb9fe15928ce1aff2554a91bc9a7aef6bf988074acf66de63128fb3034401f6238d899ab495597452e244699f9d82813d7ea729054f5b443'
+    Version = '2.0.4'
+    Url = 'https://download.microsoft.com/download/2/B/2/2B2854E7-7EAE-4FE9-85D2-19ACCD716F18/dotnet-runtime-2.0.4-win-x86.exe'
+    Checksum = '5978c622c24b8ae03e80cdd70774e90b7058908a9f380ac08cb2c7b45fde75f775eb025048af1e0fe028b2406626436019bd8dadc23868643191f86e2ff73c2a'
     ChecksumType = 'sha512'
-    Url64 = 'https://download.microsoft.com/download/5/C/1/5C190037-632B-443D-842D-39085F02E1E8/dotnet-runtime-2.0.3-win-x64.exe'
-    Checksum64 = '1575566e5bdb5f3b954e4ee4f614adfe4b47479abdb638946a3b78dc4a7f4def55b68554619947934913d095590374595c2842b9e987f331db3ba1bc0126e60d'
+    Url64 = 'https://download.microsoft.com/download/2/B/2/2B2854E7-7EAE-4FE9-85D2-19ACCD716F18/dotnet-runtime-2.0.4-win-x64.exe'
+    Checksum64 = '551a5485e9ef8af0bebea2ee82f5edc1e5c39c8a9c1d98730b963929f23dd032a885dae5877c39d1e344d9f07483ea11e3d0fdef24d33c8a8e4535281dfd9263'
     ChecksumType64 = 'sha512'
-    ApplicationName = 'Microsoft .NET Core Runtime - 2.0.3 *'
+    ApplicationName = 'Microsoft .NET Core Runtime - 2.0.4 *'
     UninstallerName = 'dotnet-runtime-*.exe'
     AdditionalArgumentsToInstaller = $null
 }
