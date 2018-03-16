2ffeb9a4e8bda4e297f9fe044e63e95b9e5d181a
martin-costello <martin.costello@just-eat.com>
2018-03-16 09:01:40 +0000

    .NET Core 2.0.6 and SDK 2.1.101
    Update for .NET Core runtime 2.0.6 and .NET Core SDK 2.1.101.

--- dotnetcore-runtime.install/tools/data.ps1
+++ dotnetcore-runtime.install/tools/data.ps1
@@ -1,13 +1,13 @@
 ﻿@{
     PackageName = 'dotnetcore-runtime.install'
-    Version = '2.0.5'
+    Version = '2.0.6'
     Url = 'https://download.microsoft.com/download/8/D/A/8DA04DA7-565B-4372-BBCE-D44C7809A467/dotnet-runtime-2.0.6-win-x86.exe'
-    Checksum = ''
+    Checksum = 'a74bab1888f6ae973c35d78c21f382c5945a0f1d6ecce10f2d8e9219b8e400f4db7600f3a6f8d580e0b0ed853a40b9f8d81a77492e30d21188f793841be62aea'
     ChecksumType = 'sha512'
     Url64 = 'https://download.microsoft.com/download/8/D/A/8DA04DA7-565B-4372-BBCE-D44C7809A467/dotnet-runtime-2.0.6-win-x64.exe'
-    Checksum64 = ''
+    Checksum64 = 'd5c185982e31fe6dd5baa01c45fe2e75d36a962b189511a69591639da9501a9ba9ac9a8e6b94160a06acb22953e235c5fb14b4a120e62537272fda872482fc4e'
     ChecksumType64 = 'sha512'
-    ApplicationName = 'Microsoft .NET Core Runtime - 2.0.5 *'
+    ApplicationName = 'Microsoft .NET Core Runtime - 2.0.6 *'
     UninstallerName = 'dotnet-runtime-*.exe'
     AdditionalArgumentsToInstaller = $null
 }
--- dotnetcore-runtime.portable/tools/data.ps1
+++ dotnetcore-runtime.portable/tools/data.ps1
@@ -1,10 +1,10 @@
 ﻿@{
     PackageName = 'dotnetcore-runtime.portable'
-    Version = '2.0.4'
-    Url = 'https://download.microsoft.com/download/A/B/1/AB1AA972-8F2F-43AD-9A81-72E9245CB0F5/dotnet-runtime-2.1.0-preview1-26216-03-win-x86.zip'
-    Checksum = '1c6c6a6821eb5e3435fa57c2661584c4118a5a951f5f7df8087922f692859ee230566dc806b9a48e8dceed703f157cac3ae3619c645e18db487a8f740db4ed95'
+    Version = '2.0.6'
+    Url = 'https://download.microsoft.com/download/8/D/A/8DA04DA7-565B-4372-BBCE-D44C7809A467/dotnet-runtime-2.0.6-win-x86.zip'
+    Checksum = 'ba29f7386f4d4adfd81649798cca730e4e8d40c4cf041e084bcfb3ee8823a789d4fada73c6ff1e323d376cf8cc3704a8b0c3d18515445b1246a98c99a1c9c087'
     ChecksumType = 'sha512'
-    Url64 = 'https://download.microsoft.com/download/A/B/1/AB1AA972-8F2F-43AD-9A81-72E9245CB0F5/dotnet-runtime-2.1.0-preview1-26216-03-win-x64.zip'
-    Checksum64 = '511447ea54aab82b9b890936440fe08a0c25e7b579050ec7706bf9212a58a31cfc56420a9de8775a7bf3bc1e39143045c542617c065e59b471323eb00567f9d4'
+    Url64 = 'https://download.microsoft.com/download/8/D/A/8DA04DA7-565B-4372-BBCE-D44C7809A467/dotnet-runtime-2.0.6-win-x64.zip'
+    Checksum64 = 'c139ee4f8f65581d908efad230111948abb29431c26d058995386d796c71890423ef0654fbc6791ac6e1a889ba2c0bb755c58ac08232d9d009da3e7d55da60d1'
     ChecksumType64 = 'sha512'
 }
