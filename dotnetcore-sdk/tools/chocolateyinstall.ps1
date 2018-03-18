57db5bb8a3c1f803f2f7608463a59e914df6afb9
Chocolatey <chocolatey@realdimensions.net>
2018-03-18 00:12:19 +0000

    AU: 2 updated - dotnetcore-runtime.install dotnetcore-sdk [skip ci]

--- dotnetcore-sdk/tools/chocolateyinstall.ps1
+++ dotnetcore-sdk/tools/chocolateyinstall.ps1
@@ -3,10 +3,10 @@ $ErrorActionPreference = 'Stop';
 
 $packageName= 'dotnetcore-sdk'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://download.microsoft.com/download/D/C/F/DCFA73BE-93CE-4DA0-AB76-98972FD6E475/dotnet-sdk-2.1.101-win-x86.exe'
-$checksum   = '22527e8d3ce51189707c411bf67a23d3745bd39f994b67d2a5004f3c143439d3583d7df3935b89f5a3d58ac5a51970c2ee61043897258d45c3fff74e95091fe0'
-$url64      = 'https://download.microsoft.com/download/D/C/F/DCFA73BE-93CE-4DA0-AB76-98972FD6E475/dotnet-sdk-2.1.101-win-x64.exe'
-$checksum64 = 'c7fb50ca82acba7e9454d53bf9c47a240611f731ea4a095957dfe58ab0404d4f98585951258fb353b5ff16a808dc89931f30b6b8a0ea6e1c967f2e84ba6ee101'
+$url        = 'https://download.microsoft.com/download/6/5/F/65F1653E-F835-4DE3-BB36-F324D3925F32/dotnet-dev-win-x86.1.1.8.exe'
+$checksum   = '8cc35987ea38aba18ab314fd41bbe5eaa3a07e97e81e69c15de4fb49efd7d3abc61901da27c6ca4bea1bde1a6ead512ddf64c89599797653dddb73041f508ee0'
+$url64      = 'https://download.microsoft.com/download/6/5/F/65F1653E-F835-4DE3-BB36-F324D3925F32/dotnet-dev-win-x64.1.1.8.exe'
+$checksum64 = '278304c8ef455e3c0d24d5b9dc3c2d0019b1a429d968e4a39f499b8e63636685ef6cfef38a50b954fa24189c3421f9a3d157a0113064af09bc55f66f391a0b8f'
 
 $packageArgs = @{
   packageName   = $packageName
