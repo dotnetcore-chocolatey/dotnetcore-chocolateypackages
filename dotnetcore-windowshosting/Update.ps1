29b8506ec94ba27015d7bf79ffce8752af1e4ccd
Manuel Riezebosch <mriezebosch@gmail.com>
2018-05-08 16:58:26 +0200

    2.1-preview -> rc & enabled 1.1 stream on windows hosting

--- dotnetcore-windowshosting/Update.ps1
+++ dotnetcore-windowshosting/Update.ps1
@@ -36,9 +36,9 @@ function global:au_GetLatest {
 
       @{
          Streams = [ordered] @{
-             '2.1' = EntryToData($json | where { $_.'version-runtime' -match '^2.1.\d+-preview.*$' } | sort { $_.'version-runtime' -as [version] } -Descending | select -First 1)
+             '2.1' = EntryToData($json | where { $_.'version-runtime' -match '^2.1.\d+-rc\d+$' } | sort { $_.'version-runtime' -as [version] } -Descending | select -First 1)
              '2.0' = EntryToData($json | where { $_.'version-runtime' -match '^2.0.\d+$' } | sort { $_.'version-runtime' -as [version] } -Descending | select -First 1)
-             #'1.1' = EntryToData($json | where { $_.'version-runtime' -match '^1.1.\d+$' } | sort { $_.'version-runtime' -as [version] } -Descending | select -First 1)
+             '1.1' = EntryToData($json | where { $_.'version-runtime' -match '^1.1.\d+$' } | sort { $_.'version-runtime' -as [version] } -Descending | select -First 1)
         }
     }
 }
