d33e8eba992d1bac74033bc505c0c30b991fc189
Jakub Bereżański <j.berezanski@medicalgorithmics.com>
2018-05-10 00:35:57 +0200

    make Get-RemoteChecksumFast shareable by multiple packages

--- dotnetcore-windowshosting/Update.ps1
+++ dotnetcore-windowshosting/Update.ps1
@@ -1,3 +1,4 @@
+. $PSScriptRoot\..\functions.ps1
 . $PSScriptRoot\..\aspnetcore-runtimepackagestore\update.ps1
 
 Rename-Item -Path Function:\au_GetLatest -NewName Get-RuntimePackageStoreLatest
@@ -29,29 +30,6 @@ function global:au_SearchReplace {
         }
     }
     return $replacements
-}
-
-function Get-RemoteChecksumFast( [string] $Url, $Algorithm='sha256' ) {
-    $pp = $ProgressPreference
-    $act = "Obtaining checksum of $Url"
-    Write-Progress -Activity $act -CurrentOperation 'Creating temporary file'
-    $fn = [System.IO.Path]::GetTempFileName()
-    Write-Progress -Activity $act -CurrentOperation 'Downloading remote file'
-    $ProgressPreference = 'SilentlyContinue'
-    try
-    {
-        Invoke-WebRequest $Url -OutFile $fn -UseBasicParsing
-    }
-    finally
-    {
-        $ProgressPreference = $pp
-    }
-    Write-Progress -Activity $act -CurrentOperation 'Computing the checksum of downloaded file'
-    $res = Get-FileHash $fn -Algorithm $Algorithm | % Hash
-    Write-Progress -Activity $act -CurrentOperation 'Removing temporary file'
-    rm $fn -ea ignore
-    Write-Progress -Activity $act -Completed
-    return $res.ToLower()
 }
 
 function global:au_BeforeUpdate() {
---
+++ functions.ps1
@@ -0,0 +1,24 @@
+﻿# based on Get-RemoteChecksum from AU, with different progress indication
+# (progress shown by Invoke-WebRequest slows it down tremendously)
+function Get-RemoteChecksumFast( [string] $Url, $Algorithm='sha256' ) {
+    $pp = $ProgressPreference
+    $act = "Obtaining checksum of $Url"
+    Write-Progress -Activity $act -CurrentOperation 'Creating temporary file'
+    $fn = [System.IO.Path]::GetTempFileName()
+    Write-Progress -Activity $act -CurrentOperation 'Downloading remote file'
+    $ProgressPreference = 'SilentlyContinue'
+    try
+    {
+        Invoke-WebRequest $Url -OutFile $fn -UseBasicParsing
+    }
+    finally
+    {
+        $ProgressPreference = $pp
+    }
+    Write-Progress -Activity $act -CurrentOperation 'Computing the checksum of downloaded file'
+    $res = Get-FileHash $fn -Algorithm $Algorithm | % Hash
+    Write-Progress -Activity $act -CurrentOperation 'Removing temporary file'
+    rm $fn -ea ignore
+    Write-Progress -Activity $act -Completed
+    return $res.ToLower()
+}
