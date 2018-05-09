d52dabec524173a3c672f4f971610b9c795a81a1
Jakub Bereżański <j.berezanski@medicalgorithmics.com>
2018-05-09 21:58:17 +0200

    dotnetcore-windowshosting: redesign obtaining of runtimepackagestore version
    This will make it possible to support runtimepackagestore streams.

--- dotnetcore-windowshosting/Update.ps1
+++ dotnetcore-windowshosting/Update.ps1
@@ -1,18 +1,10 @@
 . $PSScriptRoot\..\aspnetcore-runtimepackagestore\update.ps1
 
-function global:au_SearchReplace {
-    @{
-         "$PSScriptRoot\$($Latest.PackageName).nuspec" = @{
-             "(\<dependency .+?`"aspnetcore-runtimepackagestore`" version=)`"([^`"]+)`"" = "`$1`"[$($Latest.Version)]`""
-         }
-     }
- }
- 
- if ($MyInvocation.InvocationName -ne '.') { update -ChecksumFor none }
+Rename-Item -Path Function:\au_GetLatest -NewName Get-RuntimePackageStoreLatest
 
 $releases = "https://raw.githubusercontent.com/dotnet/core/master/release-notes/releases.json"
 function global:au_SearchReplace {
-    @{
+    $replacements = @{
         "$PSScriptRoot\tools\data.ps1" = @{
             "(^\s*Url\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"           #1
             "(^\s*Checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"      #2
@@ -20,25 +12,44 @@ function global:au_SearchReplace {
             "(^\s*Checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"      #2
 
         }
+         "$PSScriptRoot\$($Latest.PackageName).nuspec" = @{
+             "(\<dependency .+?""aspnetcore-runtimepackagestore"" version=)""([^""]+)""" = "`$1""[$($Latest.RpsVersion)]"""
+         }
     }
+    if ($Latest.RpsVersion -ne $null)
+    {
+        $replacements["$PSScriptRoot\$($Latest.PackageName).nuspec"] = @{
+             "(\<dependency .+?""aspnetcore-runtimepackagestore"" version=)""([^""]+)""" = "`$1""[$($Latest.RpsVersion)]"""
+        }
+    }
+    else
+    {
+        $replacements["$PSScriptRoot\$($Latest.PackageName).nuspec"] = @{
+             "<dependency .+?""aspnetcore-runtimepackagestore""[^>]+>" = ''
+        }
+    }
+    return $replacements
 }
 
-function EntryToData($info) {
+function EntryToData($info, $rpsVersion) {
     $version = $info.'version-runtime'
     $url32   = $info.'dlc-runtime' + $info.'hosting-win-x64.exe'
     $url64   = $info.'dlc-runtime' + $info.'hosting-win-x64.exe'
 
-     @{ Version = $version; URL32 = $url32; URL64 = $url64; ChecksumType32 = 'sha512'; ChecksumType64 = 'sha512'; }
+     @{ Version = $version; URL32 = $url32; URL64 = $url64; ChecksumType32 = 'sha512'; ChecksumType64 = 'sha512'; RpsVersion = $rpsVersion }
 }
 
 function global:au_GetLatest {
+    $rpsLatest = Get-RuntimePackageStoreLatest
+    $rpsLatest | Format-List -Property * | Out-String | Write-Debug
+
      $json = (Invoke-WebRequest -Uri $releases -UseBasicParsing | ConvertFrom-Json)
 
       @{
          Streams = [ordered] @{
-             '2.1' = EntryToData($json | where { $_.'version-runtime' -match '^2.1.\d+-rc\d+$' } | sort { $_.'version-runtime' -as [version] } -Descending | select -First 1)
-             '2.0' = EntryToData($json | where { $_.'version-runtime' -match '^2.0.\d+$' } | sort { $_.'version-runtime' -as [version] } -Descending | select -First 1)
-             '1.1' = EntryToData($json | where { $_.'version-runtime' -match '^1.1.\d+$' } | sort { $_.'version-runtime' -as [version] } -Descending | select -First 1)
+             '2.1' = EntryToData ($json | where { $_.'version-runtime' -match '^2.1.\d+-rc\d+$' } | sort { $_.'version-runtime' -as [version] } -Descending | select -First 1) $rpsLatest.Version
+             '2.0' = EntryToData ($json | where { $_.'version-runtime' -match '^2.0.\d+$' } | sort { $_.'version-runtime' -as [version] } -Descending | select -First 1) $rpsLatest.Version
+             '1.1' = EntryToData ($json | where { $_.'version-runtime' -match '^1.1.\d+$' } | sort { $_.'version-runtime' -as [version] } -Descending | select -First 1) $null
         }
     }
 }
