3d35c8f537e0811101785654fd2ad5b20b6b0c69
Jakub Bereżański <j.berezanski@medicalgorithmics.com>
2018-05-09 22:46:06 +0200

    dotnetcore-windowshosting: use wildcards for uninstall parameters

--- dotnetcore-windowshosting/tools/data.ps1
+++ dotnetcore-windowshosting/tools/data.ps1
@@ -6,7 +6,7 @@
     Url64 = 'https://download.microsoft.com/download/D/0/B/D0B7F62D-9C5D-4CF3-AB6C-88F56B4FC1A9/dotnet-hosting-2.1.0-rc1-final-win.exe'
     Checksum64 = '45ae5913e8fca08513fef71bd9a849dabd829bbaf5fbd6e9489ffeb38177e65b1042e267243d40bd70df404ccd292c15746ecd2c820ad35527308b83da4bddc0'
     ChecksumType64 = 'sha512'
-    ApplicationName = 'Microsoft .NET Core 2.0.6 - Windows Server Hosting'
-    UninstallerName = 'DotNetCore.2.0.6-WindowsHosting.exe'
+    ApplicationName = 'Microsoft .NET Core * - Windows Server Hosting'
+    UninstallerName = 'DotNetCore.*-WindowsHosting.exe'
     AdditionalArgumentsToInstaller = 'OPT_INSTALL_LTS_REDIST=0 OPT_INSTALL_FTS_REDIST=0'
 }
