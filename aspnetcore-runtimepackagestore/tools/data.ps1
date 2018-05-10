3e56d5d28a4d1d681276001d6f007df06f1f4681
Chocolatey <chocolatey@realdimensions.net>
2018-05-10 00:09:56 +0000

    AU: 2 updated - aspnetcore-runtimepackagestore dotnetcore-windowshosting [skip ci]

--- aspnetcore-runtimepackagestore/tools/data.ps1
+++ aspnetcore-runtimepackagestore/tools/data.ps1
@@ -1,10 +1,10 @@
 ﻿@{
     PackageName = 'aspnetcore-runtimepackagestore'
-    Url = 'https://download.microsoft.com/download/A/9/F/A9F8872C-48B2-41DB-8AAD-D5908D988592/AspNetCore.2.0.7.RuntimePackageStore_x86.exe'
-    Checksum = '071d19f2d897a0fcd6d61e1ea8c418b305ece143eaf1c0ad03af9de03cf49084d17408f2edb3e950b9ac3bff24638638865e88a6e149bf6f7d37f8d5f9ec22d6'
+    Url = 'https://download.microsoft.com/download/D/0/B/D0B7F62D-9C5D-4CF3-AB6C-88F56B4FC1A9/aspnetcore-runtime-2.1.0-rc1-final-win-x86.exe'
+    Checksum = 'b5e9a89ecfbd638c0f92bbb1626183def620c22e50169fa58a7a21faa83a2d48ef8f3cdc9b2815527e8885617db3624e25a5c63cdb23cf73ecd07056afe997f9'
     ChecksumType = 'sha512'
-    Url64 = 'https://download.microsoft.com/download/A/9/F/A9F8872C-48B2-41DB-8AAD-D5908D988592/AspNetCore.2.0.7.RuntimePackageStore_x64.exe'
-    Checksum64 = '81d8bb363a7d8b517027229fce7d60800902b84921dbf926582213e3b0f9b856daeba4cfed8f6ab52974f04752722303a09e2370a9cb3dae2c443fad188dbe05'
+    Url64 = 'https://download.microsoft.com/download/D/0/B/D0B7F62D-9C5D-4CF3-AB6C-88F56B4FC1A9/aspnetcore-runtime-2.1.0-rc1-final-win-x64.exe'
+    Checksum64 = '1138f7e2a23cf0309cb53986e219d52a27cba30d8d5caa3697317b4cab951bf2976211f0894eaf60900da778b4aa790490c417041abe9bc741695a259ff94789'
     ChecksumType64 = 'sha512'
     AdditionalArgumentsToInstaller = $null
 }
