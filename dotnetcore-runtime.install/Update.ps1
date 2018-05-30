00f53230d8ec386618774c80ae1cae5ddfce45cc
martincostello <martin@martincostello.com>
2018-05-30 14:18:25 +0100

    Updates for .NET Core 2.1.0
    Update versions for the final .NET Core 2.1.0 release.

--- dotnetcore-runtime.install/Update.ps1
+++ dotnetcore-runtime.install/Update.ps1
@@ -27,7 +27,7 @@ function global:au_GetLatest {
 
       @{
          Streams = [ordered] @{
-             '2.1' = EntryToData($json | where { $_.'version-runtime' -match '^2.1.\d+-rc\d+$' } | sort { $_.'version-runtime' -as [version] } -Descending | select -First 1)
+             '2.1' = EntryToData($json | where { $_.'version-runtime' -match '^2.1.\d+$' } | sort { $_.'version-runtime' -as [version] } -Descending | select -First 1)
              '2.0' = EntryToData($json | where { $_.'version-runtime' -match '^2.0.\d+$' } | sort { $_.'version-runtime' -as [version] } -Descending | select -First 1)
              '1.1' = EntryToData($json | where { $_.'version-runtime' -match '^1.1.\d+$' } | sort { $_.'version-runtime' -as [version] } -Descending | select -First 1)
              '1.0' = EntryToData($json | where { $_.'version-runtime' -match '^1.0.\d+$' } | sort { $_.'version-runtime' -as [version] } -Descending | select -First 1)
