4c345eaa9f3574e1ca61312cb3e311b81c4e55a5
Jakub Bereżański <kuba@berezanscy.pl>
2018-05-31 00:05:11 +0200

    dotnetcore-runtime.install: remove uninstall script
    The uninstall scripts need maintenance (strings get changed between
    software releases) and Chocolatey is perfectly capable of uninstalling
    this via the auto uninstaller.
    
    The same was done earlier to other packages in
    717fe1e61aaa95c363a8c7f1ca33e15cdbd50dcc.

--- dotnetcore-runtime.install/tools/data.ps1
+++ dotnetcore-runtime.install/tools/data.ps1
@@ -7,7 +7,5 @@
     Url64 = 'https://download.microsoft.com/download/9/1/7/917308D9-6C92-4DA5-B4B1-B4A19451E2D2/dotnet-runtime-2.1.0-win-x64.exe'
     Checksum64 = 'ec23c90c95d8501b05b87c42cdd189174f7c2ee8891196ee595f31e209e2670625e6b0b30d019cb09fbf4e3f17b20c9669ca63b5082cc6f8e547802eed9fffbc'
     ChecksumType64 = 'sha512'
-    ApplicationName = "Microsoft .NET Core Runtime - $env:ChocolateyPackageVersion *"
-    UninstallerName = 'dotnet-runtime-*.exe'
     AdditionalArgumentsToInstaller = $null
 }
