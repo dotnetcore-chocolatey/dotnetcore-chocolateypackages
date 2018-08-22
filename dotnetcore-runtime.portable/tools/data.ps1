9e1094ca1577f616348980d3cda96576473bafc5
Chocolatey <chocolatey@realdimensions.net>
2018-08-22 18:34:44 +0000

    AU: 1 updated - dotnetcore-runtime.portable [skip ci] https://gist.github.com/5330bb96fd3bbe3c727d9f59e96c2f95/f9a6ab127b3da48c95aead5099f250b86b414bdb

--- dotnetcore-runtime.portable/tools/data.ps1
+++ dotnetcore-runtime.portable/tools/data.ps1
@@ -1,10 +1,10 @@
 ﻿@{
     PackageName = 'dotnetcore-runtime.portable'
     Version = '2.0.6'
-    Url = 'https://download.microsoft.com/download/1/f/7/1f7755c5-934d-4638-b89f-1f4ffa5afe89/dotnet-runtime-2.1.2-win-x86.zip'
-    Checksum = 'e0e941c2b89b8638447e42f93a0a028cd81464ac4cea6123acb4130b72dda03744b3bcbb42683f1e9554b1cadd1c28f35d97523278f40de5e8c57606460556b8'
+    Url = 'https://download.microsoft.com/download/6/E/B/6EBD972D-2E2F-41EB-9668-F73F5FDDC09C/dotnet-runtime-2.1.3-win-x86.zip'
+    Checksum = 'c9dd70d7b98297d32de05108efc2f3859f31fcc4c92a20e8797c51df7d14608280185dbdce25d172a56bfc935562fa66677497c48b8115d92245be3e4bc6fa5b'
     ChecksumType = 'sha512'
-    Url64 = 'https://download.microsoft.com/download/1/f/7/1f7755c5-934d-4638-b89f-1f4ffa5afe89/dotnet-runtime-2.1.2-win-x64.zip'
-    Checksum64 = '9e67c62feb34aeb52a45d0e623d1ad098637a322545956eb5e6de2287ddad2412b766c492ae5a7dddc123a4cb47cfc51d9bb10d0e30c007ec3fc90666f9733c8'
+    Url64 = 'https://download.microsoft.com/download/6/E/B/6EBD972D-2E2F-41EB-9668-F73F5FDDC09C/dotnet-runtime-2.1.3-win-x64.zip'
+    Checksum64 = 'eec3465a1dfecc3890e8c07d45198352a9ef4026bebc867ca8723ddf4bf30e83b79dfae4c5b8800a4b1b83d38c63be06b8ebf7c5dce87cf7108cbb83b169b47f'
     ChecksumType64 = 'sha512'
 }
