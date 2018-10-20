169bf5f12f8a722d7b0f664dce1cda6ae5ab805a
Manuel Riezebosch <mriezebosch@gmail.com>
2018-10-20 09:33:28 +0200

    sdk: uninstall using registry

--- dotnetcore-sdk/tools/chocolateyuninstall.ps1
+++ dotnetcore-sdk/tools/chocolateyuninstall.ps1
@@ -1,40 +1,34 @@
-﻿$ErrorActionPreference = 'Stop';
-
-$packageName= 'dotnetcore-sdk'
-$silentArgs = "/uninstall /quiet /norestart"
-$url        = 'https://go.microsoft.com/fwlink/?LinkID=798399'
-$url64bit   = 'https://go.microsoft.com/fwlink/?LinkID=798398'
-$fileType   = "exe"
-
-# Resolving the download location in the same way as Install-ChocolateyPackage: 
-#   https://github.com/chocolatey/choco/blob/master/src/chocolatey.resources/helpers/functions/Install-ChocolateyPackage.ps1
-$chocTempDir = $env:TEMP
-$tempDir = Join-Path $chocTempDir "$packageName"
-if ($env:packageVersion -ne $null) {$tempDir = Join-Path $tempDir "$env:packageVersion"; }
-
-if (![System.IO.Directory]::Exists($tempDir)) { [System.IO.Directory]::CreateDirectory($tempDir) | Out-Null }
-$file = Join-Path $tempDir "$($packageName)Install.$fileType"
-
-$filePath = Get-ChocolateyWebFile $packageName $file $url $url64bit -checksum $checksum -checksumType $checksumType -checksum64 $checksum64 -checksumType64 $checksumType64 -options $options -getOriginalFileName
-
-# Supporting chocolatey 0.9.9.x where the original/full path was not returned by Get-ChocolateyWebFile
-if ($filePath -eq $null) { $filePath = $file }
-
-Uninstall-ChocolateyPackage $packageName "EXE" $silentArgs $filePath -validExitCodes @(0, 3010)
 
 
 
+$ErrorActionPreference = 'Stop';
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  softwareName  = 'Microsoft .NET Core SDK*'
+  fileType      = 'EXE'
+  silentArgs    = "/uninstall /quiet"
+  validExitCodes= @(0, 3010, 1605, 1614, 1641)
+}
 
+$uninstalled = $false
+[array]$key = Get-UninstallRegistryKey -SoftwareName $packageArgs['softwareName'] | 
+    ?{ -Not $_.WindowsInstaller -eq 1 } # for some reason the SDK is registered twice but picking the EXE instead of MSI here
 
+if ($key.Count -eq 1) {
+  $key | % { 
+    $file = $_.UninstallString -replace '(?<=".+")\s+.*$', ''
+    Write-Output "File: $file"
+    $packageArgs['file'] = $file
 
-
-
-
-
-
-
-
-
-
+    Uninstall-ChocolateyPackage @packageArgs
+  }
+} elseif ($key.Count -eq 0) {
+  Write-Warning "$packageName has already been uninstalled by other means."
+} elseif ($key.Count -gt 1) {
+  Write-Warning "$($key.Count) matches found!"
+  Write-Warning "To prevent accidental data loss, no programs will be uninstalled."
+  Write-Warning "Please alert package maintainer the following keys were matched:"
+  $key | % {Write-Warning "- $($_.DisplayName)"}
+}
 
 
