f8b1b384a5eee3b99fcfded5d13fabc74557fcc6
Chocolatey <chocolatey@realdimensions.net>
2018-12-07 00:12:58 +0000

    AU: 1 updated - dotnetcore-sdk [skip ci] https://gist.github.com/5330bb96fd3bbe3c727d9f59e96c2f95/077cd797039713c52401ebd4a00958f283396b3e

--- dotnetcore-sdk/tools/chocolateyinstall.ps1
+++ dotnetcore-sdk/tools/chocolateyinstall.ps1
@@ -3,10 +3,10 @@ $ErrorActionPreference = 'Stop';
 
 $packageName= 'dotnetcore-sdk'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://download.visualstudio.microsoft.com/download/pr/c7aecc9d-e8d0-451b-a5ed-de3095459883/9fcfdce401be67e0e53eee337e6c82c4/dotnet-sdk-2.2.100-win-x86.exe'
-$checksum   = 'F961FB0197FDF0EBE345D34BC25B9FAAC3804E60DA8A2AA7E0C859951CF64E05913F302779CD0FF1AD49582F925EC11FAAA27E83C21AD656B8EE87900CBC923D'
-$url64      = 'https://download.visualstudio.microsoft.com/download/pr/7ae62589-2bc1-412d-a653-5336cff54194/b573c4b135280fb369e671a8f477163a/dotnet-sdk-2.2.100-win-x64.exe'
-$checksum64 = 'CDA96523F5969B8A338FFB89A14972F2D8B5697E60B7BC48BE70E019BB70BEEBF4EAAE2F3E84E0F9A90040EBDBA4E781D525CF7B41B95D44DAABD6A862129F7C'
+$url        = 'https://download.visualstudio.microsoft.com/download/pr/9182f8b1-0b48-45da-ba29-ff2e06ad07ce/2c4a3dc1958867a54b1abe1a73ccbcac/dotnet-sdk-2.2.200-preview-009648-win-x86.exe'
+$checksum   = '552528AF370039032C8A5AE17D55904DEBAA4523BD62C56E9946C0D0A84610DFB78271BA4575C34E92FC8F1A985CCF62FC85D51903FFFDABB80C3D336044D04D'
+$url64      = 'https://download.visualstudio.microsoft.com/download/pr/8145cebd-ea45-4b3d-b13a-9f37c7be0588/00b4a7ed7952412edcaee68e181d72c4/dotnet-sdk-2.2.200-preview-009648-win-x64.exe'
+$checksum64 = 'ED41A4AABBE13117566AFB00F08C582E62833E40F755082E6BE8F135EF880A0586478A88148BE99EF6F4BA4D812E0C7C6833A12A3FDB1CA4DEE238EA58B8F091'
 
 $packageArgs = @{
   packageName   = $packageName
