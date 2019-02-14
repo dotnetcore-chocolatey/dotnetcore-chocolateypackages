30bc55edfac2656bce61a12b3ce8fab5726beaf8
Chocolatey <chocolatey@realdimensions.net>
2019-02-14 18:06:13 +0000

    AU: 1 updated - dotnetcore-runtime.portable [skip ci] https://gist.github.com/5330bb96fd3bbe3c727d9f59e96c2f95/043f7f009c4c28d2beb9028c91344aaa7bf9a683

--- dotnetcore-runtime.portable/tools/data.ps1
+++ dotnetcore-runtime.portable/tools/data.ps1
@@ -1,10 +1,10 @@
 ﻿@{
     PackageName = 'dotnetcore-runtime.portable'
     Version = '2.0.6'
-    Url = 'https://download.visualstudio.microsoft.com/download/pr/ab4bbec2-ace5-487b-85fd-4ac897440040/23e50fbfc65d3b54aec21107eeb0a66d/dotnet-runtime-2.2.0-win-x86.zip'
-    Checksum = 'f426a8b8baeb0e34a9b9cde257d10ca59ee934b95708ad42bf1fa6fb12cd97e6727ecafdbdbc2edcae7b275a063478f5fac3dd77fedca62c874d8932df8f65fb'
+    Url = 'https://download.visualstudio.microsoft.com/download/pr/97b777ee-6b4e-4d26-9f73-6b33a99e0f67/93458d5a38d673757768eed2f6cec926/dotnet-runtime-2.2.2-win-x86.zip'
+    Checksum = 'c44066da16fee653d33e4e40ef2a695c4bd193d4f5d30ac3fa666a3e971b1b27ff148d474f26cb3f2befab6851d99add613267e642ba8610187584ff5c51a458'
     ChecksumType = 'sha512'
-    Url64 = 'https://download.visualstudio.microsoft.com/download/pr/62711024-fa98-4919-9fe0-466744b20941/4cdef0431350a441b45e11784f657b09/dotnet-runtime-2.2.0-win-x64.zip'
-    Checksum64 = '5b262c6d30ebeace5718d063363ccf778c49cfed535cc5baaff2d518ca94cfe83ffbdacb8eb0e3617d5cfcc3d07d3c1f7ceed0b26636f6971ba91054d78bc1f1'
+    Url64 = 'https://download.visualstudio.microsoft.com/download/pr/b10d0a68-b720-48ae-bab8-4ac39bd1b5d3/f32b8b41dff5c1488c2b915a007fc4a6/dotnet-runtime-2.2.2-win-x64.zip'
+    Checksum64 = 'e8813de402b66e461ecf1e81ee327338374adb6279bc0876bef1dc84b1fbf2f94a716434ed40e6b0f9af1cfaa85256aa439e6c7765a9ef13dffdef08a3ef6097'
     ChecksumType64 = 'sha512'
 }
