e50ba7ccadf5c9dfb2d311853ed7deef6756661c
Chocolatey <chocolatey@realdimensions.net>
2019-07-23 18:06:20 +0000

    AU: 4 updated - dotnetcore dotnetcore-runtime dotnetcore-runtime.install dotnetcore-sdk [skip ci] https://gist.github.com/5330bb96fd3bbe3c727d9f59e96c2f95/6c2b541ec9a6dab759aa6ca3f99df2498990714b

--- dotnetcore-runtime.install/tools/data.ps1
+++ dotnetcore-runtime.install/tools/data.ps1
@@ -1,11 +1,11 @@
 ﻿@{
     PackageName = 'dotnetcore-runtime.install'
     Version = '2.1.0'
-    Url = 'https://download.visualstudio.microsoft.com/download/pr/f9b622df-fc7d-4ec6-8996-b03ad7553c93/910152992719bfee5011c722ae82c680/dotnet-runtime-2.2.6-win-x86.exe'
-    Checksum = '886FB3A42954D8181A77429140B6DEA49B00783D48CDBB0AEE621F2436C4DD09D6E43EE08FE98CC5EDE9B17A630DFF0FB68761F3EAD74673200066FEB83F0E5C'
+    Url = 'https://download.visualstudio.microsoft.com/download/pr/5dd5f4db-81b6-42d0-9a9d-b6457ef231b8/68800a8d75cc2dc92b0816b4acdeee33/dotnet-runtime-3.0.0-preview7-27912-14-win-x86.exe'
+    Checksum = '85A26B51AF8EBAAD857437C9E7D6D23CE3CC1AB637FD8569CDCAC9990A3449D2FCE1E25E0C70B18F418A4AE8C78C97898990A8133BBBD492D87D50EE91B8318B'
     ChecksumType = 'sha512'
-    Url64 = 'https://download.visualstudio.microsoft.com/download/pr/e65be1d4-dbe3-460c-8517-3fafb26b3dc4/e7760a9dbb9135e3b0b0150f36ef0f05/dotnet-runtime-2.2.6-win-x64.exe'
-    Checksum64 = 'C6C399D009E2D742C55E467E351EDF23E64832D8559E6A07005215DB465A75DFE08A1B46828C58EB9929C6051161555F790EB6059FBD1F046CE67A7F3091E57D'
+    Url64 = 'https://download.visualstudio.microsoft.com/download/pr/9ef148e5-e1e2-46a5-a0bf-c556cc999204/d0e0882b1fb57b7fcf850e80ada3df4f/dotnet-runtime-3.0.0-preview7-27912-14-win-x64.exe'
+    Checksum64 = '4FCBFD36C501C22A3116325FC0E6B00CD88C6D52560FCF8390416BAFEE042EF9F5607A11ED37864895DBB550EC4D769BAF6E0CC3F88AC66605D5BCB5FFD929E5'
     ChecksumType64 = 'sha512'
     AdditionalArgumentsToInstaller = $null
 }
--- dotnetcore-sdk/tools/chocolateyinstall.ps1
+++ dotnetcore-sdk/tools/chocolateyinstall.ps1
@@ -3,10 +3,10 @@ $ErrorActionPreference = 'Stop';
 
 $packageName= 'dotnetcore-sdk'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://download.visualstudio.microsoft.com/download/pr/4a6fef07-dc91-4fbd-87c8-74fd8db71aeb/9fa79398a69ac4bd08e8bf51080b3553/dotnet-sdk-3.0.100-preview6-012264-win-x86.exe'
-$checksum   = '89E27B9A43EACE572883452565321F09B0104B25303083FA61A5A6BE464EC51705582CF281069F526E3FF7487208E8E6B0BC71FD6D8DEA32B617571435EA0A95'
-$url64      = 'https://download.visualstudio.microsoft.com/download/pr/4d2dfaa1-4f9c-4526-bb6f-117d9d8bbd0e/a9fc9994c1b4d485ab41632b81bf4f56/dotnet-sdk-3.0.100-preview6-012264-win-x64.exe'
-$checksum64 = 'D53279FD8D6D816E65C134F52C2211E27D6C5BBBD8A8AE9A1D7008CFD1A9143A97A033676B9CDB9B86CC1CF50F430D6BD5A7646250AD1DC4A18D3FCD75AD4D8B'
+$url        = 'https://download.visualstudio.microsoft.com/download/pr/e7e10509-f1ec-4d5c-9fe9-33a2d5a8fac0/dcf905cdac05719a5a5fa1ee1c365c4e/dotnet-sdk-3.0.100-preview7-012821-win-x86.exe'
+$checksum   = '98D71FD29875C645FF07C5DB9B4BD5985C96F282CCDD8C47DAC59A88B8571CE30BFAA8E6AA13640D53515D35FADEA02AB8565E083C807496EEC150C5F05ECBCA'
+$url64      = 'https://download.visualstudio.microsoft.com/download/pr/a65e3536-ad76-4808-9920-83702aeed082/3c6ab9eaa0bc99df442be91e7b7950ff/dotnet-sdk-3.0.100-preview7-012821-win-x64.exe'
+$checksum64 = '3A94FA4CB71072E401697F3D6601AD01997D814079862B9AC4A2DB12288C603D5EB91E6523F9C443F10CA753C5FA9182E1D355FD097A736D43E8EACBAB05DE52'
 
 $packageArgs = @{
   packageName   = $packageName
