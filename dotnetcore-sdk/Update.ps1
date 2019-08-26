0f2183e5a17eb4cff54ffa22cb59d627da105d79
David Gardiner <david@gardiner.net.au>
2019-08-26 11:00:24 +0930

    Adding additional versions
    - Support new 'sdks' array
    
    Implements #43

--- dotnetcore-sdk/Update.ps1
+++ dotnetcore-sdk/Update.ps1
@@ -19,13 +19,24 @@ function EntryToData() {
     if (-Not($version)) {
         $version = $result."latest-sdk"
     }
-    $latest = $result.releases | ?{ $_.sdk.version -like $version } | select -First 1
 
-    $exe64 = $latest.sdk.files | ?{ $_.name -like '*win-x64.exe' }
-    $exe32 = $latest.sdk.files | ?{ $_.name -like '*win-x86.exe' }
+    # try sdks array first
+    $latest = $result.releases.sdks | Where-Object { $_.version -like $version } | Select-Object -First 1
+
+    if ($latest) {
+        $exe64 = $latest.files | Where-Object { $_.name -like '*win-x64.exe' }
+        $exe32 = $latest.files | Where-Object { $_.name -like '*win-x86.exe' }
+        $latestVersion = $latest.version
+    } else {
+        $latest = $result.releases | Where-Object { $_.sdk.version -like $version } | Select-Object -First 1
+
+        $exe64 = $latest.sdk.files | Where-Object { $_.name -like '*win-x64.exe' }
+        $exe32 = $latest.sdk.files | Where-Object { $_.name -like '*win-x86.exe' }
+        $latestVersion = $latest.sdk.version
+    }
 
     @{ 
-        Version = $latest.sdk.version;
+        Version = $latestVersion;
         URL32 = $exe32.url;
         URL64 = $exe64.url;
         ChecksumType32 = 'sha512';
@@ -41,7 +52,18 @@ function global:au_GetLatest {
              '3.0' = EntryToData -channel '3.0'
              '2.2' = EntryToData -channel '2.2'
              '2.2.1' = EntryToData -channel '2.2' -version '2.2.1??'
+             '2.2.2' = EntryToData -channel '2.2' -version '2.2.2??'
+             '2.2.3' = EntryToData -channel '2.2' -version '2.2.3??'
+             '2.2.4' = EntryToData -channel '2.2' -version '2.2.4??'
              '2.1' = EntryToData -channel '2.1'
+             '2.1.3' = EntryToData -channel '2.1' -version '2.1.3??'
+             '2.1.4' = EntryToData -channel '2.1' -version '2.1.4??'
+             '2.1.5' = EntryToData -channel '2.1' -version '2.1.5??'
+             '2.1.6' = EntryToData -channel '2.1' -version '2.1.6??'
+             '2.1.7' = EntryToData -channel '2.1' -version '2.1.7??'
+             '2.1.8' = EntryToData -channel '2.1' -version '2.1.8??'
+             '2.1.1' = EntryToData -channel '2.0' -version '2.1.1??' # 2.1.10x SDK is for 2.0
+             '2.1.2' = EntryToData -channel '2.0' -version '2.1.2??' # 2.1.20x SDK is for 2.0
              '1.1' = EntryToData -channel '1.1'
         }
     }
