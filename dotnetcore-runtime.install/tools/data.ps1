254ab64d181d2ec7a825bd56d9f8c66cfd684354
Chocolatey <chocolatey@realdimensions.net>
2019-12-03 18:08:43 +0000

    AU: 4 updated - aspnetcore-runtimepackagestore dotnetcore-runtime dotnetcore-runtime.install dotnetcore-sdk [skip ci] https://gist.github.com/5330bb96fd3bbe3c727d9f59e96c2f95/c52f3dfcfb0e6fdf728b87c27d48c7b96f713a4f

--- dotnetcore-runtime.install/tools/data.ps1
+++ dotnetcore-runtime.install/tools/data.ps1
@@ -1,11 +1,11 @@
 ﻿@{
     PackageName = 'dotnetcore-runtime.install'
     Version = '2.1.0'
-    Url = 'https://download.visualstudio.microsoft.com/download/pr/8dd9ad38-1a35-45d1-9a27-f07eb599b5db/5b2d47f567f27e3b38145fd294e01b2b/dotnet-runtime-3.0.1-win-x86.exe'
-    Checksum = '6d22f4619e812fbd5d7101cebd0a9ffdaee9986cd841fe67e1fb81126a7d59fda6e05a6cb750ff6b7ad668874e3a367c28e8f68f27650abd7dba5e5b169b10d6'
+    Url = 'https://download.visualstudio.microsoft.com/download/pr/10256f2e-4665-4acb-918b-26c7c85956cd/e562428eb91b894e711cd81b53166575/dotnet-runtime-3.1.0-win-x86.exe'
+    Checksum = '60c3952c1ab4c97a296921a5d6573caccca2581aaf9b0f22a2b9f767cae33cb9f8c369d2b0fff35c7bd6cfbc1c753e201cc663630f2f5ae52624d71b2e55b6f1'
     ChecksumType = 'sha512'
-    Url64 = 'https://download.visualstudio.microsoft.com/download/pr/d12cc6fa-8717-4424-9cbf-d67ae2fb2575/b4fff475e67917918aa2814d6f673685/dotnet-runtime-3.0.1-win-x64.exe'
-    Checksum64 = '757d3441a20ce96e33472966f94212862d9246688ac9d20d89519c173e0bada324dbc6845247c29977da77db5257782a0a082741f158d836cce443ec28900d4e'
+    Url64 = 'https://download.visualstudio.microsoft.com/download/pr/71c181aa-e8fc-4581-ab8b-93a5906b75ff/7458c330a88227463eb0721aded45426/dotnet-runtime-3.1.0-win-x64.exe'
+    Checksum64 = 'a0221b55437fb87b3e16a11aa92b06dd6dec6a10e036f627644c35e16b04372d8511d6c0ad8b81a5af37414769f3dd769dcf2a2a3e56d4cb0fd10fbad5c914c7'
     ChecksumType64 = 'sha512'
     AdditionalArgumentsToInstaller = $null
 }
