c9624e85ff24c4d870a2008b3703490d007fad3d
Jerome Bell <jeromebell@gmail.com>
2019-12-04 09:32:06 -0600

    Fixed 3.1.0 links;

--- dotnetcore-runtime.install/tools/data.ps1
+++ dotnetcore-runtime.install/tools/data.ps1
@@ -1,11 +1,11 @@
 ﻿@{
     PackageName = 'dotnetcore-runtime.install'
     Version = '2.1.0'
-    Url = 'https://download.visualstudio.microsoft.com/download/pr/10256f2e-4665-4acb-918b-26c7c85956cd/e562428eb91b894e711cd81b53166575/dotnet-runtime-3.1.0-win-x86.exe'
-    Checksum = '60c3952c1ab4c97a296921a5d6573caccca2581aaf9b0f22a2b9f767cae33cb9f8c369d2b0fff35c7bd6cfbc1c753e201cc663630f2f5ae52624d71b2e55b6f1'
+    Url = 'https://download.visualstudio.microsoft.com/download/pr/04356520-4cf7-4e98-b4a8-b3043e2eeca3/cdf6d3d104334dfe8f471fe135f16b07/dotnet-runtime-3.1.0-win-x86.exe'
+    Checksum = 'f9c1c8dabc46a28e07ae88b2ca94495c9b9091c15d82678e79b86bf02fa007ad16998201e6e308818c54891abff36afb6f5536994cec4ea2bb76bc61253e8f2d'
     ChecksumType = 'sha512'
-    Url64 = 'https://download.visualstudio.microsoft.com/download/pr/71c181aa-e8fc-4581-ab8b-93a5906b75ff/7458c330a88227463eb0721aded45426/dotnet-runtime-3.1.0-win-x64.exe'
-    Checksum64 = 'a0221b55437fb87b3e16a11aa92b06dd6dec6a10e036f627644c35e16b04372d8511d6c0ad8b81a5af37414769f3dd769dcf2a2a3e56d4cb0fd10fbad5c914c7'
+    Url64 = 'https://download.visualstudio.microsoft.com/download/pr/9f010da2-d510-4271-8dcc-ad92b8b9b767/d2dd394046c20e0563ce5c45c356653f/dotnet-runtime-3.1.0-win-x64.exe'
+    Checksum64 = 'd4d6eef858776856482e23769f2a1653a941830d9ec8b6661d2f7b5e97c80a3172f00da0589ab2a5a23fae0858c38cd132403a8aa1e453b50370b0ad2c374a41'
     ChecksumType64 = 'sha512'
     AdditionalArgumentsToInstaller = $null
 }
