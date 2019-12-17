89a063c0a450a6d8f555052adf01898c8aa5ebaa
Manuel Riezebosch <mriezebosch@gmail.com>
2019-12-17 12:49:07 +0100

    feat(runtime.portable): auto-update

--- dotnetcore-runtime.portable/tools/data.ps1
+++ dotnetcore-runtime.portable/tools/data.ps1
@@ -1,10 +1,10 @@
 ﻿@{
     PackageName = 'dotnetcore-runtime.portable'
-    Version = '2.0.6'
-    Url = 'https://download.visualstudio.microsoft.com/download/pr/0281cdac-cfb3-442c-bdfb-abe1b8010bf1/fbbcb7fa128f2bffdec3de28d17e53f4/dotnet-runtime-2.2.3-win-x86.zip'
-    Checksum = '01e4a3efcf257ce8b5ebdeef1185ea0e7c1f359be7d6a1f826e6b1a74041fe67f450f3472adbd9507177060c135ce45dee6051d0680b9671f256686f28afcef6'
+    Version = '3.1.0'
+    Url = 'https://download.visualstudio.microsoft.com/download/pr/31b707c9-0484-48b5-b248-7f22946f88b5/a998787f1b26a7f742c84cbec7f145d2/dotnet-runtime-3.1.0-win-x86.zip'
+    Checksum = 'fdd9223585731a5bcc5540a5913f9578debad2339b41d9a1fdee09cbd6136ac7435b281f8f2c700346c235a96737b2464da164f4393861ab13aeb5b8465acb56'
     ChecksumType = 'sha512'
-    Url64 = 'https://download.visualstudio.microsoft.com/download/pr/25d53223-179f-46db-b99d-5d433c93a021/dd1f391be09111440b3afe38d22bc15d/dotnet-runtime-2.2.3-win-x64.zip'
-    Checksum64 = 'bd065bd434d4bffa0c22605b3d3d12d150eae41ae45272d51696c8609ff0a83e0f1a147a789db7afaa78e8a02b6b167b79b7e5056f21aa0c1168bea5d14c4451'
+    Url64 = 'https://download.visualstudio.microsoft.com/download/pr/5e1c20ea-113f-47fd-9702-22a8bf1e3974/16bf234b587064709d8e7b58439022d4/dotnet-runtime-3.1.0-win-x64.zip'
+    Checksum64 = '90a534dd82c99d659373a7549c87dad4dc1ff9d3074e355785ee0f9db6022d785913b019f42652b3a4906d2963f08d2c86cdd95fb77a7ad7ccfefd626ad51490'
     ChecksumType64 = 'sha512'
 }
