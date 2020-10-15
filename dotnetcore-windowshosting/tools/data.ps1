e96af01a6ab6aadb60017c54e9b4e15ce354fb60
Chocolatey <chocolatey@realdimensions.net>
2020-10-15 18:52:30 +0000

    AU: 1 updated - dotnetcore-windowshosting [skip ci] https://gist.github.com/5330bb96fd3bbe3c727d9f59e96c2f95/aa1be646bc43af78fa9a82d4523c2fef0d9df70b

--- dotnetcore-windowshosting/tools/data.ps1
+++ dotnetcore-windowshosting/tools/data.ps1
@@ -1,10 +1,10 @@
 ﻿@{
     PackageName = 'dotnetcore-windowshosting'
-    Url = 'https://download.visualstudio.microsoft.com/download/pr/3e3c37fb-4d77-4558-a78c-17434e1cc804/60116643f610fb43f858af4e0dc1b223/dotnet-hosting-2.1.23-win.exe'
-    Checksum = '704eaa7595abf5bb4ffb5bbd45fd64f3d6e1a2f856f61b2fb746913d9f10da9133cc44be4e75d9e012bf0db6e5cb390d3af59b85593aeb1c29ae020a7c7c2117'
+    Url = 'https://download.visualstudio.microsoft.com/download/pr/beca42b0-54a8-4364-86b8-a3d88003fbb7/592e0eec1e5e53f78d9647f7112cc743/dotnet-hosting-3.1.9-win.exe'
+    Checksum = '674d5f7d12a3fd8070fe5faccda982f5a52a69321cd5018e22d47dfe7cb565667d3402ce1b14b1cf8df1eafcbc9ce56e8c078ed926a273bdc357674e92c221fd'
     ChecksumType = 'sha512'
-    Url64 = 'https://download.visualstudio.microsoft.com/download/pr/3e3c37fb-4d77-4558-a78c-17434e1cc804/60116643f610fb43f858af4e0dc1b223/dotnet-hosting-2.1.23-win.exe'
-    Checksum64 = '704eaa7595abf5bb4ffb5bbd45fd64f3d6e1a2f856f61b2fb746913d9f10da9133cc44be4e75d9e012bf0db6e5cb390d3af59b85593aeb1c29ae020a7c7c2117'
+    Url64 = 'https://download.visualstudio.microsoft.com/download/pr/beca42b0-54a8-4364-86b8-a3d88003fbb7/592e0eec1e5e53f78d9647f7112cc743/dotnet-hosting-3.1.9-win.exe'
+    Checksum64 = '674d5f7d12a3fd8070fe5faccda982f5a52a69321cd5018e22d47dfe7cb565667d3402ce1b14b1cf8df1eafcbc9ce56e8c078ed926a273bdc357674e92c221fd'
     ChecksumType64 = 'sha512'
     AdditionalArgumentsToInstaller = 'OPT_INSTALL_LTS_REDIST=0 OPT_INSTALL_FTS_REDIST=0'
 }
