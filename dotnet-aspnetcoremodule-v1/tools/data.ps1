1faabf90f677c5e320b097bf41921558ca539cb0
Chocolatey <chocolatey@realdimensions.net>
2020-11-06 08:10:26 +0000

    AU: 3 updated - dotnet dotnet-aspnetcoremodule-v1 dotnet-aspnetcoremodule-v2 [skip ci] https://gist.github.com/5330bb96fd3bbe3c727d9f59e96c2f95/497c970d9468304e1b3197e45dca46afef6a396e

--- dotnet-aspnetcoremodule-v1/tools/data.ps1
+++ dotnet-aspnetcoremodule-v1/tools/data.ps1
@@ -1,10 +1,10 @@
 ﻿@{
-    PackageName = 'tbd'
-    Url = ''
-    Checksum = ''
-    ChecksumType = 'tbd'
-    Url64 = ''
-    Checksum64 = ''
-    ChecksumType64 = 'tbd'
+    PackageName = 'dotnet-aspnetcoremodule-v1'
+    Url = 'https://download.visualstudio.microsoft.com/download/pr/ba001109-03c6-45ef-832c-c4dbfdb36e00/e3413f9e47e13f1e4b1b9cf2998bc613/dotnet-hosting-2.2.8-win.exe'
+    Checksum = '1b3177fc65ec343f641b8ffdc2a0e925e322e90ed44dcb5c6d3982a370dd7b56f7fcc362dad3a4b7e2db4f0fe6878b7e7448fc7f41dfe01302c7484434691f6b'
+    ChecksumType = 'sha512'
+    Url64 = 'https://download.visualstudio.microsoft.com/download/pr/ba001109-03c6-45ef-832c-c4dbfdb36e00/e3413f9e47e13f1e4b1b9cf2998bc613/dotnet-hosting-2.2.8-win.exe'
+    Checksum64 = '1b3177fc65ec343f641b8ffdc2a0e925e322e90ed44dcb5c6d3982a370dd7b56f7fcc362dad3a4b7e2db4f0fe6878b7e7448fc7f41dfe01302c7484434691f6b'
+    ChecksumType64 = 'sha512'
     AdditionalArgumentsToInstaller = 'OPT_NO_RUNTIME=1 OPT_NO_SHAREDFX=1' # these arguments are valid for .NET Core 2.1+
 }
--- dotnet-aspnetcoremodule-v2/tools/data.ps1
+++ dotnet-aspnetcoremodule-v2/tools/data.ps1
@@ -1,10 +1,10 @@
 ﻿@{
-    PackageName = 'tbd'
-    Url = ''
-    Checksum = ''
-    ChecksumType = 'tbd'
-    Url64 = ''
-    Checksum64 = ''
-    ChecksumType64 = 'tbd'
+    PackageName = 'dotnet-aspnetcoremodule-v2'
+    Url = 'https://download.visualstudio.microsoft.com/download/pr/03f2261a-01cc-4a78-8505-88ba0457ef29/1ff22faa58152bec84e636e719c28528/dotnet-hosting-5.0.0-rc.2.20475.17-win.exe'
+    Checksum = '49ac516fe6fa24bafcfb71be5fe8480d107c474a10bdd5d4da33c5d16e7e22bd42f9757f678ff8c681ccf82e579bcdda2edfd988a01e2c48940b21aaace86d6b'
+    ChecksumType = 'sha512'
+    Url64 = 'https://download.visualstudio.microsoft.com/download/pr/03f2261a-01cc-4a78-8505-88ba0457ef29/1ff22faa58152bec84e636e719c28528/dotnet-hosting-5.0.0-rc.2.20475.17-win.exe'
+    Checksum64 = '49ac516fe6fa24bafcfb71be5fe8480d107c474a10bdd5d4da33c5d16e7e22bd42f9757f678ff8c681ccf82e579bcdda2edfd988a01e2c48940b21aaace86d6b'
+    ChecksumType64 = 'sha512'
     AdditionalArgumentsToInstaller = 'OPT_NO_RUNTIME=1 OPT_NO_SHAREDFX=1'
 }
