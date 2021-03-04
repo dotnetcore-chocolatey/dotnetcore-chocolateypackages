74718ee8d4d9f6697c5a267e6bc82b2c764b4f3d
Jakub Bereżański <kuba@berezanscy.pl>
2021-03-04 21:55:57 +0100

    Add dotnet-5.0-sdk-2xx

---
+++ dotnet-5.0-sdk-2xx/tools/data.ps1
@@ -0,0 +1,10 @@
+﻿@{
+    PackageName = 'dotnet-5.0-sdk-2xx'
+    Url = 'https://download.visualstudio.microsoft.com/download/pr/a9111d43-d9af-4aae-bf81-8a335fd7f490/5041619a227cb1fe473f3738b6b2e8fe/dotnet-sdk-5.0.200-win-x86.exe'
+    Checksum = '773a81dabff01369b208d3234f526bddb8837b2307cb4d65cdad6e8b708655f6dbeb8e71001967d72270434fb36d74621d2ebe2cb85fb70182f60935db46a46f'
+    ChecksumType = 'sha512'
+    Url64 = 'https://download.visualstudio.microsoft.com/download/pr/a105fe06-20a0-4233-8ff1-b85523b40f1d/5f26654016c41ab2dc6d8bc850a9bf4c/dotnet-sdk-5.0.200-win-x64.exe'
+    Checksum64 = 'ad82e39229f7c1b59578e86b29318a82bfff96831e7d845fb3044e9eed9c40d2963d1ccb8c23d02ace1be9f1591909a366dec9230332c6328875ab5dcc00f53c'
+    ChecksumType64 = 'sha512'
+    AdditionalArgumentsToInstaller = $null
+}
