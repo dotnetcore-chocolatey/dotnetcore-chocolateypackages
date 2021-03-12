f1b4490649a1079b039426dfbd16445e7859feed
Chocolatey <chocolatey@realdimensions.net>
2021-03-12 06:07:02 +0000

    AU: 1 updated - dotnet-5.0-sdk-2xx [skip ci] https://gist.github.com/5330bb96fd3bbe3c727d9f59e96c2f95

--- dotnet-5.0-sdk-2xx/tools/data.ps1
+++ dotnet-5.0-sdk-2xx/tools/data.ps1
@@ -1,10 +1,10 @@
 ﻿@{
     PackageName = 'dotnet-5.0-sdk-2xx'
-    Url = 'https://download.visualstudio.microsoft.com/download/pr/a9111d43-d9af-4aae-bf81-8a335fd7f490/5041619a227cb1fe473f3738b6b2e8fe/dotnet-sdk-5.0.200-win-x86.exe'
-    Checksum = '773a81dabff01369b208d3234f526bddb8837b2307cb4d65cdad6e8b708655f6dbeb8e71001967d72270434fb36d74621d2ebe2cb85fb70182f60935db46a46f'
+    Url = 'https://download.visualstudio.microsoft.com/download/pr/e18b7dc0-21b2-49b0-82ce-f487601fab50/cab80970bb51bc8b9b3d67c430a1e58b/dotnet-sdk-5.0.201-win-x86.exe'
+    Checksum = '761d86868f6af57111a87eac05f7f25e945d69ea04b989f6d1d9b207823319a9280a07ab60885cff4a397d50e8b48063e812aef825a16001e51179bb855ae223'
     ChecksumType = 'sha512'
-    Url64 = 'https://download.visualstudio.microsoft.com/download/pr/a105fe06-20a0-4233-8ff1-b85523b40f1d/5f26654016c41ab2dc6d8bc850a9bf4c/dotnet-sdk-5.0.200-win-x64.exe'
-    Checksum64 = 'ad82e39229f7c1b59578e86b29318a82bfff96831e7d845fb3044e9eed9c40d2963d1ccb8c23d02ace1be9f1591909a366dec9230332c6328875ab5dcc00f53c'
+    Url64 = 'https://download.visualstudio.microsoft.com/download/pr/78a6328f-f563-4a7f-a478-3ed0f2ce8ec6/5beb762f64d8a018a5b9e590bc1531e0/dotnet-sdk-5.0.201-win-x64.exe'
+    Checksum64 = 'aa1b4466415129b1d6ce5fced473f2a4092d457e5f3e8d36546eb79ab515fb9683aef5e0162230986950a38da4ba27b6fb3caa75771348dc57793d4f4ca2065c'
     ChecksumType64 = 'sha512'
     AdditionalArgumentsToInstaller = $null
 }
