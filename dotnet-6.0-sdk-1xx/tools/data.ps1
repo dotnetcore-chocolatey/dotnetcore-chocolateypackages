e4174902d588ccf4a52a7cf43b6781e4ec1631b3
Chocolatey <chocolatey@realdimensions.net>
2021-09-22 18:07:13 +0000

    AU: 3 updated - dotnet-6.0-sdk dotnet-6.0-sdk-1xx dotnet-sdk [skip ci] https://gist.github.com/5330bb96fd3bbe3c727d9f59e96c2f95/6fa6045bcd2907bcfa2b6eeb2007aeedfe07c247

--- dotnet-6.0-sdk-1xx/tools/data.ps1
+++ dotnet-6.0-sdk-1xx/tools/data.ps1
@@ -1,10 +1,10 @@
 ﻿@{
     PackageName = 'dotnet-6.0-sdk-1xx'
-    Url = 'https://download.visualstudio.microsoft.com/download/pr/1f5f4e58-82b6-48e6-8d4d-607556294a21/c3bc9c334581b82c98cbdb2691a6ffd0/dotnet-sdk-6.0.100-rc.1.21458.32-win-x86.exe'
-    Checksum = '8c75b449748b7b1478ef12ee8d82c69a67cc1a0fcb13f19126650d6be58a2a167b48c91cfe35fb294bc9fe8e927cd0a5aec1adca5e87b4fd927780bd9da55368'
+    Url = 'https://download.visualstudio.microsoft.com/download/pr/0cc5e1ae-c744-4a5e-a950-28bd3149da86/8f2db99d1aa77594928356fde7467586/dotnet-sdk-6.0.100-rc.1.21463.6-win-x86.exe'
+    Checksum = 'da0c7334050a781eb93d0660ce71e54e7a4d81ca73b83634a245e13f0d26aea985eceedd63a5c1a2ce41e107ea244a4c45f33dd83ca7747f8515c85cdd49183a'
     ChecksumType = 'sha512'
-    Url64 = 'https://download.visualstudio.microsoft.com/download/pr/3a85672a-1662-4a71-b781-d103ce66c465/b074f9908930fb2266aaf9f3da166386/dotnet-sdk-6.0.100-rc.1.21458.32-win-x64.exe'
-    Checksum64 = '36f8e44ea470bbc4a5c883ca6cb799ba2b7be2e73cd8ca2b547bad83d554ae7aeb839ed801e5a8d757634681bd408cc52c4f8c0ad1b870153e93b39f1c1c9811'
+    Url64 = 'https://download.visualstudio.microsoft.com/download/pr/d214c16c-4485-441b-b4e8-50867f370ff9/159f2e1223b494b909ce2288fc2bc6c5/dotnet-sdk-6.0.100-rc.1.21463.6-win-x64.exe'
+    Checksum64 = '28874389ca1085ea9567c0e6fdb35c897980242fc1d22a7bbc139fbaf0726aafd0910202e47850cf4e36a3b25ef74856e09c6169f4915b7709f9fdf79aaee509'
     ChecksumType64 = 'sha512'
     AdditionalArgumentsToInstaller = $null
 }
