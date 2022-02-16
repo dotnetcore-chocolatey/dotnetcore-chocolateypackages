20b3aab420a000f03a0dbe40d4296bc774ed7125
dotnetcore-chocolateypackages <dotnetcore-chocolateypackages@outlook.com>
2022-02-16 00:08:40 +0000

    AU: 3 updated - dotnet-6.0-sdk dotnet-6.0-sdk-2xx dotnet-sdk [skip ci] https://gist.github.com/0b5fb8ff0bf938a89b7fb6d48f988ea3/ed86feede4c8b3658bd0ff383e28a47d646603eb

---
+++ dotnet-6.0-sdk-2xx/tools/data.ps1
@@ -0,0 +1,10 @@
+﻿@{
+    PackageName = 'dotnet-6.0-sdk-2xx'
+    Url = 'https://download.visualstudio.microsoft.com/download/pr/a3e3a0a4-1eca-442b-8745-f6f6e1ace489/a2e866259154c9f094e4b3779ac15fc4/dotnet-sdk-6.0.200-win-x86.exe'
+    Checksum = 'b25aba0a2662cfb16124741a0ae4c14b279f9056ffb2f7802463238a0508e0e33c5d912664fd1a05e3f7d21ec02527b480fd5aaeed3d7e3acb0f3bf323ca73dd'
+    ChecksumType = 'sha512'
+    Url64 = 'https://download.visualstudio.microsoft.com/download/pr/89f0ba2a-5879-417b-ba1d-debbb2bde208/b22a9e9e4d513e4d409d2222315d536b/dotnet-sdk-6.0.200-win-x64.exe'
+    Checksum64 = '101de40bf82fb8e17e966e788b7c80f560fa6eb55dce98d959f3671092cf4be1e659d9d11debc6fd7d3b6f09f5c5d5c3adf55ee79a038071bb60e144211180eb'
+    ChecksumType64 = 'sha512'
+    AdditionalArgumentsToInstaller = $null
+}
