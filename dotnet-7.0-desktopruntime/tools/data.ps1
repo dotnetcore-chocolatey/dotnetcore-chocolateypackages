d96d0614f745bee95158489ab6606916abccf3e5
Jakub Bereżański <kuba@berezanscy.pl>
2022-02-18 22:18:16 +0100

    Add .NET 7.0 package stubs

---
+++ dotnet-7.0-desktopruntime/tools/data.ps1
@@ -0,0 +1,10 @@
+﻿@{
+    PackageName = 'dotnet-7.0-desktopruntime'
+    Url = 'tbd'
+    Checksum = 'tbd'
+    ChecksumType = 'sha512'
+    Url64 = 'tbd'
+    Checksum64 = 'tbd'
+    ChecksumType64 = 'sha512'
+    AdditionalArgumentsToInstaller = $null
+}
