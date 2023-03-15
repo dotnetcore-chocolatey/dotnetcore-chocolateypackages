f7655f9fd7e02402065fba1e6aa593997d42da0f
dotnetcore-chocolateypackages <dotnetcore-chocolateypackages@outlook.com>
2023-03-15 06:09:32 +0000

    AU: 6 updated - dotnet-aspnetcoremodule-v2 dotnet-aspnetruntime dotnet-desktopruntime dotnet-runtime dotnet-sdk dotnet-windowshosting [skip ci] https://gist.github.com/0b5fb8ff0bf938a89b7fb6d48f988ea3/b645c5908baae02954ba7c45aa69b171876ac24d

--- dotnet-aspnetcoremodule-v2/tools/data.ps1
+++ dotnet-aspnetcoremodule-v2/tools/data.ps1
@@ -1,10 +1,10 @@
 ﻿@{
     PackageName = 'dotnet-aspnetcoremodule-v2'
-    Url = 'https://download.visualstudio.microsoft.com/download/pr/09af9a4b-7cd4-425e-b9ce-25579faaf528/99930a6816270440fee1268a6652cb34/dotnet-hosting-8.0.0-preview.2.23153.2-win.exe'
-    Checksum = 'c88859e805120dfdd65fc8869aa2c80e28c8ed1cb0f3c9a62e29463c1f52fc161836529885a64d7917330f023b042cc14eb314055f4fb48e961d4e698676f1ef'
+    Url = 'https://download.visualstudio.microsoft.com/download/pr/e38901ef-e9ac-4331-a6aa-f2aec3b1754b/6d695fa51a4960393edaf725ce970a86/dotnet-hosting-6.0.15-win.exe'
+    Checksum = '0fcec40e3d4ee131dcdf8ee606a603274e71921fbaa3c0f3a7b5fdb4a1b001ce86e46ae0840b2e58ce1a930797ff68b274a46630ab7995e01633c8a6dcee15ba'
     ChecksumType = 'sha512'
-    Url64 = 'https://download.visualstudio.microsoft.com/download/pr/09af9a4b-7cd4-425e-b9ce-25579faaf528/99930a6816270440fee1268a6652cb34/dotnet-hosting-8.0.0-preview.2.23153.2-win.exe'
-    Checksum64 = 'c88859e805120dfdd65fc8869aa2c80e28c8ed1cb0f3c9a62e29463c1f52fc161836529885a64d7917330f023b042cc14eb314055f4fb48e961d4e698676f1ef'
+    Url64 = 'https://download.visualstudio.microsoft.com/download/pr/e38901ef-e9ac-4331-a6aa-f2aec3b1754b/6d695fa51a4960393edaf725ce970a86/dotnet-hosting-6.0.15-win.exe'
+    Checksum64 = '0fcec40e3d4ee131dcdf8ee606a603274e71921fbaa3c0f3a7b5fdb4a1b001ce86e46ae0840b2e58ce1a930797ff68b274a46630ab7995e01633c8a6dcee15ba'
     ChecksumType64 = 'sha512'
     AdditionalArgumentsToInstaller = 'OPT_NO_RUNTIME=1 OPT_NO_SHAREDFX=1'
 }
