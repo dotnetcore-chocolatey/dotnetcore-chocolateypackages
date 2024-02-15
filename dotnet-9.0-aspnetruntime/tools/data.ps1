ae5535edc51a966f461f7aae7486a400e37292c9
Jakub Bereżański <kuba@berezanscy.pl>
2024-02-15 20:02:25 +0100

    Add .NET 9.0

---
+++ dotnet-9.0-aspnetruntime/tools/data.ps1
@@ -0,0 +1,10 @@
+﻿@{
+    PackageName = 'dotnet-9.0-aspnetruntime'
+    Url = 'TBD'
+    Checksum = 'TBD'
+    ChecksumType = 'sha512'
+    Url64 = 'TBD'
+    Checksum64 = 'TBD'
+    ChecksumType64 = 'sha512'
+    AdditionalArgumentsToInstaller = $null
+}
