4fec0b13e01d72b1d94d3ecdf30d9aa40c22933c
dotnetcore-chocolateypackages <dotnetcore-chocolateypackages@outlook.com>
2024-12-04 00:10:57 +0000

    AU: 3 updated - dotnet-9.0-sdk dotnet-9.0-sdk-1xx dotnet-sdk [skip ci] https://gist.github.com/dotnetcore-chocolateypackages/0b5fb8ff0bf938a89b7fb6d48f988ea3/367552064d612512ca5c6e889261a57b9ba0dc4e

--- dotnet-9.0-sdk-1xx/tools/data.ps1
+++ dotnet-9.0-sdk-1xx/tools/data.ps1
@@ -1,10 +1,10 @@
 ﻿@{
     PackageName = 'dotnet-9.0-sdk-1xx'
-    Url = 'https://download.visualstudio.microsoft.com/download/pr/9ac55860-8ff9-4506-b005-d88df3092c01/bd204d9ae6b3ca66dab904f1169729a3/dotnet-sdk-9.0.100-win-x86.exe'
-    Checksum = '854053f68672d922740e82849b7608ecdf63019fbcaee5970aac097a33b743f76df281e64b298c0bf138d0043aa01b3cd9627199f7fcb6a523210d6ad086a90d'
+    Url = 'https://download.visualstudio.microsoft.com/download/pr/838bc4f1-96ee-43f9-8e47-2dc2656590c0/15cbe313b18ccbeffdb61cff66f5ef26/dotnet-sdk-9.0.101-win-x86.exe'
+    Checksum = '14d22475bf0d13b01d36f472353239de0351454f3b570d0629f6003aab55032ff575fae237e20d2e9e3d45a9f6a884c6d1330e025ebcf22a8e19d68d7f64c149'
     ChecksumType = 'sha512'
-    Url64 = 'https://download.visualstudio.microsoft.com/download/pr/10bb041d-e705-473e-9654-27c0e038f5bd/447c0c10654c2949872fa6154b8c27b5/dotnet-sdk-9.0.100-win-x64.exe'
-    Checksum64 = 'a12ee028f7dff8f330dbe1914534d237eb6e19cc105139ce5de69df1b4b07ee3c1a3e396574ca776a452e805052e799df14a348ace50191af514c9dc4705ecf0'
+    Url64 = 'https://download.visualstudio.microsoft.com/download/pr/38e45a81-a6a4-4a37-a986-bc46be78db16/33e64c0966ebdf0088d1a2b6597f62e5/dotnet-sdk-9.0.101-win-x64.exe'
+    Checksum64 = '6c1899452dc855698ccc2a9928301352e5700e7829f0d42a1e567b51f08089affc67801ba7cd49d7e45b4a4dcb79cba54561163d64c27c3f36108737b3bf9f62'
     ChecksumType64 = 'sha512'
     AdditionalArgumentsToInstaller = $null
 }
