73e6ed6e4a9213bd7805c2a3aa4f9139c32c1952
Jakub Bereżański <kuba@berezanscy.pl>
2025-02-26 01:20:58 +0100

    Add .NET 10

---
+++ dotnet-10.0-aspnetruntime/tools/data.ps1
@@ -0,0 +1,10 @@
+﻿@{
+    PackageName = 'dotnet-10.0-aspnetruntime'
+    Url = 'TBD'
+    Checksum = 'TBD'
+    ChecksumType = 'sha512'
+    Url64 = 'TBD'
+    Checksum64 = 'TBD'
+    ChecksumType64 = 'sha512'
+    AdditionalArgumentsToInstaller = $null
+}
