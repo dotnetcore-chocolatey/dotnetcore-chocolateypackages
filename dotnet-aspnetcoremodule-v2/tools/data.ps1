533de797ef21edcb229fd4b959b82390fb9112e2
Jakub Bereżański <kuba@berezanscy.pl>
2025-05-30 19:45:51 +0200

    Revert "dotnet-aspnetcoremodule-v2: Work around installer bug"
    The Hosting Bundle installer problem [1] was fixed in August 2024.
    
    This reverts commit 53d3f6f6eed7ec7f8cea0bcb7768bf3e6de0686d.
    
    [1] https://github.com/dotnet/aspnetcore/issues/45395
    
    Related issues: GH-63 GH-73

--- dotnet-aspnetcoremodule-v2/tools/data.ps1
+++ dotnet-aspnetcoremodule-v2/tools/data.ps1
@@ -6,5 +6,5 @@
     Url64 = 'https://builds.dotnet.microsoft.com/dotnet/aspnetcore/Runtime/10.0.0-preview.4.25258.110/dotnet-hosting-10.0.0-preview.4.25258.110-win.exe'
     Checksum64 = '2b4084f7a291f67d0c73fab697a26d25afef3ba3ceadb14292096743e77f07d7663d20703485c3097ef2bf86f89f855f69c6c75377f87545db525a44037a8b3a'
     ChecksumType64 = 'sha512'
-    AdditionalArgumentsToInstaller = 'OPT_NO_RUNTIME=1' # OPT_NO_SHAREDFX=1 removed as a workaround for https://github.com/dotnet/aspnetcore/issues/45395
+    AdditionalArgumentsToInstaller = 'OPT_NO_RUNTIME=1 OPT_NO_SHAREDFX=1'
 }
