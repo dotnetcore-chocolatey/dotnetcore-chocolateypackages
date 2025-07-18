f7ba92bd14d015f2b98e3d051210f978827b1d4f
dotnetcore-chocolateypackages <dotnetcore-chocolateypackages@outlook.com>
2025-07-18 00:11:43 +0000

    AU: 3 updated - dotnet-9.0-sdk dotnet-9.0-sdk-3xx dotnet-sdk [skip ci] https://gist.github.com/dotnetcore-chocolateypackages/0b5fb8ff0bf938a89b7fb6d48f988ea3/08e2cfba0fd9cc421b275ecb831d8fa9b381598c

--- dotnet-9.0-sdk-3xx/tools/data.ps1
+++ dotnet-9.0-sdk-3xx/tools/data.ps1
@@ -1,10 +1,10 @@
 ﻿@{
     PackageName = 'dotnet-9.0-sdk-3xx'
-    Url = 'https://builds.dotnet.microsoft.com/dotnet/Sdk/9.0.302/dotnet-sdk-9.0.302-win-x86.exe'
-    Checksum = '018ebbced95ab79dad05f38ee72b3808e0cbdda0ae84ce358250203798fa50e7f78a45a509bcda8a17bf7b3eab0dcea8f6113bb19400290816f54c919667b7d3'
+    Url = 'https://builds.dotnet.microsoft.com/dotnet/Sdk/9.0.303/dotnet-sdk-9.0.303-win-x86.exe'
+    Checksum = 'c126b2b78b68b68843fecb197a3bb5592b57befd29729977689d11da80bf9356feefd3705c664578e1c7e5d554b060c3d1d8b1b3434837b562d524a1662085ac'
     ChecksumType = 'sha512'
-    Url64 = 'https://builds.dotnet.microsoft.com/dotnet/Sdk/9.0.302/dotnet-sdk-9.0.302-win-x64.exe'
-    Checksum64 = '0cb87affddb63171f9c79033386fef2b1fbef209a7381de46bc94876f4aee475cb2f71b6a074708c4a5a94a0a1a464cb5ba424c572f2550ce562ad0705e3691e'
+    Url64 = 'https://builds.dotnet.microsoft.com/dotnet/Sdk/9.0.303/dotnet-sdk-9.0.303-win-x64.exe'
+    Checksum64 = '96653bd5906362e28bdcd35c9a997ef0c34dc0e26e79ab62f5266a2b619ad3d00824dc88de0ae61cbe96f5f2ab86012fac968a85868161b8583881b632cd12af'
     ChecksumType64 = 'sha512'
     AdditionalArgumentsToInstaller = $null
 }
