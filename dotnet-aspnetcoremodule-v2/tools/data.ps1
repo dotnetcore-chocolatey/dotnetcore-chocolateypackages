bd1e21860ad693cfe999fd7ab2c9e1ec02d07b8d
dotnetcore-chocolateypackages <dotnetcore-chocolateypackages@outlook.com>
2025-07-25 17:33:57 +0000

    AU: 2 updated - dotnet-10.0-windowshosting dotnet-aspnetcoremodule-v2 [skip ci] https://gist.github.com/dotnetcore-chocolateypackages/0b5fb8ff0bf938a89b7fb6d48f988ea3/759d991cbe0a1804355673e08c654df59b3b1666

--- dotnet-aspnetcoremodule-v2/tools/data.ps1
+++ dotnet-aspnetcoremodule-v2/tools/data.ps1
@@ -1,10 +1,10 @@
 ﻿@{
     PackageName = 'dotnet-aspnetcoremodule-v2'
-    Url = 'https://builds.dotnet.microsoft.com/dotnet/aspnetcore/Runtime/9.0.7/dotnet-hosting-9.0.7-win.exe'
-    Checksum = '4c26a2d2c23679605dc519075830416fe96204804bdb9bd3f2e2cac786a80645fba528f5bb0432fa19b2f6a3e5d618bea833bfe859200eee099c3a728402dcf2'
+    Url = 'https://builds.dotnet.microsoft.com/dotnet/aspnetcore/Runtime/10.0.0-preview.6.25358.103/dotnet-hosting-10.0.0-preview.6.25358.103-win.exe'
+    Checksum = '753ebe5c14f67f433128a64571c6c941aec7a0888f010a2a163797f2e4f9327f90a21f112afc51de1f819ae1ee434abd09a622d2ba51d690a41b178b59d36e1d'
     ChecksumType = 'sha512'
-    Url64 = 'https://builds.dotnet.microsoft.com/dotnet/aspnetcore/Runtime/9.0.7/dotnet-hosting-9.0.7-win.exe'
-    Checksum64 = '4c26a2d2c23679605dc519075830416fe96204804bdb9bd3f2e2cac786a80645fba528f5bb0432fa19b2f6a3e5d618bea833bfe859200eee099c3a728402dcf2'
+    Url64 = 'https://builds.dotnet.microsoft.com/dotnet/aspnetcore/Runtime/10.0.0-preview.6.25358.103/dotnet-hosting-10.0.0-preview.6.25358.103-win.exe'
+    Checksum64 = '753ebe5c14f67f433128a64571c6c941aec7a0888f010a2a163797f2e4f9327f90a21f112afc51de1f819ae1ee434abd09a622d2ba51d690a41b178b59d36e1d'
     ChecksumType64 = 'sha512'
     AdditionalArgumentsToInstaller = 'OPT_NO_RUNTIME=1 OPT_NO_SHAREDFX=1'
 }
