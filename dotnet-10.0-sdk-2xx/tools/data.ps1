479f85fbadf11cb8d7a9108603eafc90a7142c22
dotnetcore-chocolateypackages <dotnetcore-chocolateypackages@outlook.com>
2026-03-19 00:11:33 +0000

    AU: 1 updated - dotnet-10.0-sdk-2xx [skip ci] https://gist.github.com/dotnetcore-chocolateypackages/0b5fb8ff0bf938a89b7fb6d48f988ea3/5834ebc2faa43c297ac57f34d6d5d7c1cb0e1ba8

--- dotnet-10.0-sdk-2xx/tools/data.ps1
+++ dotnet-10.0-sdk-2xx/tools/data.ps1
@@ -1,10 +1,10 @@
 ﻿@{
     PackageName = 'dotnet-10.0-sdk-2xx'
-    Url = 'https://builds.dotnet.microsoft.com/dotnet/Sdk/10.0.200/dotnet-sdk-10.0.200-win-x86.exe'
-    Checksum = 'bd32de8df6c68e271c83ff947151e2ff76709c4156045aff22f9d11caaef2ca4f1b542a47174492fbb8aa7a9a0bd9230b3c093a70c740b7f62411e5125874ce3'
+    Url = 'https://builds.dotnet.microsoft.com/dotnet/Sdk/10.0.201/dotnet-sdk-10.0.201-win-x86.exe'
+    Checksum = '9df1853990a8e1628ca95fc75dbbd07ebd371b77f64068aa4162831f057e53302ec49f948fde59f3bbb8eed28f4bb5566bdc7ab344352234282189bcde424147'
     ChecksumType = 'sha512'
-    Url64 = 'https://builds.dotnet.microsoft.com/dotnet/Sdk/10.0.200/dotnet-sdk-10.0.200-win-x64.exe'
-    Checksum64 = '3197e12a6eb9676a7d3a5b323c84043fe3f8d3983c4d289f907a9b43df6f3ad82921b2ae8ffb75b06630f0d3a1abdb60c48b826bbd4cb0f47b382e54aaf664e4'
+    Url64 = 'https://builds.dotnet.microsoft.com/dotnet/Sdk/10.0.201/dotnet-sdk-10.0.201-win-x64.exe'
+    Checksum64 = '4f51c56e4e9976266f8f6a44e40583bf942955a192a88ceb482fba7975a1eb8f049ae2dc6265c0b3972f1e1fea7237cc96fea46c38c0fa9c29b0b51e6aa414e4'
     ChecksumType64 = 'sha512'
     AdditionalArgumentsToInstaller = $null
 }
